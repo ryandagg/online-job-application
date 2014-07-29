5261b05a07c981564f5a68f838e1d216c850bed5
ryandagg <ryan.dagg@gmail.com>
2014-07-29 13:57:42 -0600

    Applicants page is now list of links that direct to applicant profiles. All bonuses finished.

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.get('/', function(req, res) {
 app.get('/applicants', function(req, res){
 	model.Applicant.find(function(err, users) {
 		if(err) {
-			console.log("wtf")
+			console.log("can not find applicants")
 		}else{
 			res.render('applicants', {
 				data: users
@@ -27,6 +27,20 @@ app.get('/applicants', function(req, res){
 		}
 	})
 });
+
+app.get('/_id:id?', function(req, res) {
+	// console.log("req.params.id:", req.params.id);
+	model.Applicant.find({_id: req.params.id}, function(err, user) {
+		if(err){
+			console.log("can not find applicant")
+		}else{
+			console.log("applicant:", user)
+			res.render("oneApplicant", {
+				theApplicant: user[0]
+			})
+		}
+	})
+})
 
 app.get("/success", function(req, res) {
 	res.render("success")
@@ -56,7 +70,7 @@ app.post("/delete", function(req, res) {
 		if(err) {
 			console.log("delete failed")
 		}else{
-			return console.log("user:", user);
+			return console.log("user deleted:", user);
 		}
 	})
 	res.redirect("applicants");
